7920a5b5688b585f4ae32c943e916375c2ef16c3
maipormat <mai00841@hotmail.com>
2024-11-06 10:14:05 +0700

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import reactLogo from './assets/react.svg'
+
+import './App.css'
+
+function App() {
+ 
+
+  return (
+    <>
+      <h1 className="text-3xl font-bold underline text-red-400">
+      Hello world!
+    </h1>
+    </>
+  )
+}
+
+export default App
