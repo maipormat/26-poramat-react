b4f76fe066ad5bc00c1ca1b5795e06a6a4d89329
maipormat <mai00841@hotmail.com>
2024-11-06 13:53:07 +0700

    s

--- src/Components/Owner.jsx
+++ src/Components/Owner.jsx
@@ -1,7 +1,48 @@
 function Owner() {
     return (
-        <div className="flex flex-col items-center mt-10">
-        <h1 className="text-4xl font-bold text-center">Owner Page</h1>
+      <div className="flex flex-col items-center mt-10">
+        <h1 className="text-4xl font-bold text-center">26 Poramat Mai</h1>
+        
+    
+        <img 
+          src="https://www.matichon.co.th/wp-content/uploads/2019/07/%E0%B8%99%E0%B8%B3.jpg" 
+          alt="Owner" 
+          className="mt-6 w-72 h-72 object-cover rounded-full shadow-lg"
+        />
+  
+        <p className="mt-6 text-lg text-center max-w-2xl px-4">
+        Ba ba ba ba ba ba ba Banana
+So very yummy when I take off your peel
+Ba ba ba ba ba ba ba Banana
+I like to eat you with every meal
+Pe pe pe pe pe pe pe Peach
+I like to pick you off your little tree
+Pe pe pe pe pe pe pe Peach
+You are so cute and fuzzy
+Wa wa wa wa watermelon
+You satisfy all my tasty summer needs
+Wa wa wa wa watermelon
+I love to spit out your seeds
+Ah ah ah ah ah ah ah apple
+I love to hear that very loud crunch
+Ah ah ah ah ah ah ah apple
+I like to eat you right after lunch
+Oh oh oh oh oh oh oh orange
+So very clever with your vitamin C
+Oh oh oh oh oh oh oh orange
+I can't believe you are so tasty
+Str str str str str strawberry
+You are so yummy for your little size
+Str str str str str strawberry
+I must eat you I will not compromise
+Ch ch ch ch ch ch cherry
+So fitting on top of my sundae
+Ch ch ch ch ch ch cherry
+You always seem to brighten up my day
+Ba ba ba ba ba ba ba Banana
+Now we're right back where we've began
+Ba ba ba ba ba ba ba Banana
+       </p>
       </div>
     );
   }
